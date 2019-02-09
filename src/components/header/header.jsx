29a9280386ea0d138d0c251cf9d0fd204674beff
Bruno <brunopjsw@gmail.com>
2019-02-09 00:30:21 -0200

    Creation

---
+++ src/components/header/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Header } from './index-css';
+
+export const header = () => {
+  return (
+    <React.Fragment>
+        <Header>
+          oooi
+        </Header>
+    </React.Fragment>
+  )
+}
+
+export default header
