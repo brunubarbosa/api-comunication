cc5bdbb4459ad21b8f85afed31be904ab5deb9ea
Bruno <brunopjsw@gmail.com>
2019-02-11 23:13:10 -0200

    att

--- src/App.js
+++ src/App.js
@@ -10,44 +10,12 @@ import Body from './components/body/body';
 
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state={
-      currencyName: []
-    }
-    this.processNewCurrency = this.processNewCurrency.bind(this);
-    this.request = this.request.bind(this);
-    this.request()
-  }
-  
-
-  request() {
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = () => {
-      if (xhttp.readyState == 4 && xhttp.status == 200) {
-        
-        this.processNewCurrency(xhttp.responseText)
-        }
-    };
-    xhttp.open("GET", "https://economia.awesomeapi.com.br/all", true);
-    xhttp.send();
-  }
-
-  processNewCurrency(content) {
-    let allCurrencyJSON = JSON.parse(content)
-    for(let key in allCurrencyJSON) {
-        this.setState({currencyName: this.state.currencyName.concat(key)})
-    }
-    this.setState({allCurrencyJSON: allCurrencyJSON})
-  }
- 
-
   render() {
     return (
         <React.Fragment>
           <Header>
         </Header>
-        <Body request={this.request} currencyName={this.state.currencyName} allCurrency={this.state.allCurrency} activeCurrency={this.state.activeCurrency}/>
+        <Body/>
         </React.Fragment>
     )
   }
--- src/components/body/body.jsx
+++ src/components/body/body.jsx
@@ -1,47 +1,81 @@
-import React from 'react';
+
 import {Body, BoxCenter, LeftPart, RightPart} from '../body/body-css';
 import { Row } from '../utils/row-css';
 import { FormGroup } from '../utils/form-group.jsx';
 import { Submit } from '../utils/input-css'
+import React, { Component } from 'react'
 
-
-const getValueCurrency = () => {
-        console.log()
-}
-
-export const body = (props) => {
+export class body extends Component {
     
-
+    constructor() {
+        super();
+        this.state={
+          currencyName: [],
+          currencyOBJ: '',
+          calcValue: ''
+        }
+        this.processNewCurrency = this.processNewCurrency.bind(this);
+        this.request = this.request.bind(this);
+        this.request()
+      }
+      
     
-    return(
+      request() {
+        let xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = () => {
+          if (xhttp.readyState == 4 && xhttp.status == 200) {
+            
+            this.processNewCurrency(xhttp.responseText)
+            }
+        };
+        xhttp.open("GET", "https://economia.awesomeapi.com.br/all", true);
+        xhttp.send();
+      }
+    
+      processNewCurrency(content) {
+        let allCurrencyJSON = JSON.parse(content)
+        for(let key in allCurrencyJSON) {
+            this.setState({currencyName: this.state.currencyName.concat(key)})
+        }
+        this.setState({allCurrencyJSON: allCurrencyJSON})
+      }
+
+      changeSelect(event) {
+          console.log(this.state.allCurrencyJSON)
+          this.setState({currencyOBJ: this.state.allCurrencyJSON[event.target.value]})
+      }
+
+      calcValue(event) {
+          this.setState({calcValue: this.state.currencyOBJ.bid * event.target.value})
+      }
+  render() {
+    return (
         <Body>
-            <BoxCenter>
-
-                <LeftPart />
-                <RightPart>
-                
-                    <Row style={{justifyContent: 'space-between'}}>
-                        <select onLoad={props.request}  name="" id="">
-
-                        {props.currencyName.map((element, index) => {
-                            return <option name={props.currencyName[index]}>{props.currencyName[index]}</option>
-                        })}
-                        
-                        </select>
-                    </Row>
+        <BoxCenter>
+            <LeftPart />
+            <RightPart>
+                    <select onLoad={this.state.request} onChange={(event)=>this.changeSelect(event)} name="" id="">
+                    {this.state.currencyName.map((element, index) => {
+                        return <option name={this.state.currencyName[index]}>{this.state.currencyName[index]}</option>
+                    })}
+                    </select>
+                <Row>
+                    <span>Moeda: {this.state.currencyOBJ.name}</span>
+                </Row>
+                <Row>
+                    <span>Valor: {this.state.currencyOBJ.bid}</span>
+                </Row>
+                <Row>
+                    <input type="text" onChange={(event) => {this.calcValue(event)}} name="" placeholder="digite o valor em reais" id=""/>
+                </Row>
                     <Row>
-                        <span>Um: {}</span>
+                    <span>{this.state.calcValue}</span>
                     </Row>
-                    <Row>
-                        <span>Dois: </span>
-                    </Row>
-                    <Row>
-                        <button onClick={props.request}>Enviar</button>
-                    </Row>
-                </RightPart>
-            </BoxCenter>
-        </Body>
+            </RightPart>
+        </BoxCenter>
+    </Body>
     )
+  }
 }
 
-export default body;
+export default body
